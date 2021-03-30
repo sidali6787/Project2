ff80626b8c9f6740a25cda50cfc0405576680c65
sidali6787 <sidali6787@gmail.com>
2021-03-30 17:54:54 -0400

    3rd push

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
+# from flask_pymongo import PyMongo
+# from flask import Flask, render_template, redirect
+
+# app = Flask(__name__)
+# mongo = PyMongo(app,uri='mongodb://localhost:27017/project2_db')
+
+# @app.route('/')
+# def home():
+#     data = mongo.db.restaurantes.find_one()
+#     return render_template('index.html', restaurantes=data)
+
+# if __name__=='__main__':
+#     app.run(debug=True)
+
+import pandas as pd
 from flask_pymongo import PyMongo
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 
 app = Flask(__name__)
-mongo = PyMongo(app,uri='mongodb://localhost:27017/project2_db')
+mongo = PyMongo(app, uri='mongodb://localhost:27017/project2_db')
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/api/v1.0/restaurantes')
+def get_data():
     data = mongo.db.restaurantes.find_one()
-    return render_template('index.html', restaurantes=data)
+    print(data)
+    df = pd.DataFrame(data).to_dict(orient='record')
+    return jsonify(data)
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,3 @@
+d3.json('/api/v1.0/restaurantes').then(data=>{
+    console.log(data);
+})
